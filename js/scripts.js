42d6c883a60fa39d642eea7743155123e5d0ed8a
Jesse Hoare Evans and Zach Bastian <zach@>
2017-03-27 12:40:00 -0700

    it will add a loop system to replace old system.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,25 @@
 $(document).ready(function(){
-  $("#formOne").submit(function(event){
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+  $("#formOne").submit(function(event) {
+    var loops = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    var array1 = [];
+    loops.forEach(function(loop) {
+      var userInput = $("input#" + loop).val();
+      $("." + loop).text(userInput);
+    });
 
-    array1.push(person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput);
-
-    var array2 = [array1[1] + " " + array1[2]];
-
-    $("ul").append("<li>" + array1[0] + "</li>");
-
-    console.log(array2);
-
-
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput = $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
     $("#story").show();
 
